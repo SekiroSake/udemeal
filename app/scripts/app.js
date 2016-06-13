510ee3a692add7c4059fb93f77a4e54592ce1306
Adam Wang <hwang12wou@gmail.com>
2016-06-13 14:35:23 -0400

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name udemealApp
+ * @description
+ * # udemealApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('udemealApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udemealApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the udemealApp
+ */
+angular.module('udemealApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

a858792a31be4d10cb88a4e815560a615cd90095
Adam Wang <hwang12wou@gmail.com>
2016-06-15 15:52:27 -0400

    new service ordermanager

---
+++ app/scripts/services/ordermanager.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name udemealApp.orderManager
+ * @description
+ * # orderManager
+ * Service in the udemealApp.
+ */
+angular.module('udemealApp')
+  .service('orderManager', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });

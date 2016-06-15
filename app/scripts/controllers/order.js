c36e7a4a0d21725eb18f32c0b17bb70809f0d757
Adam Wang <hwang12wou@gmail.com>
2016-06-15 16:11:11 -0400

    added meal feature

---
+++ app/scripts/controllers/order.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udemealApp.controller:OrderCtrl
+ * @description
+ * # OrderCtrl
+ * Controller of the udemealApp
+ */
+angular.module('udemealApp')
+  .controller('OrderCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- app/scripts/services/ordermanager.js
+++ app/scripts/services/ordermanager.js
@@ -10,4 +10,31 @@
 angular.module('udemealApp')
   .service('orderManager', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    var OrderSelection = {
+      Monday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+      Tuesday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+      Wednesday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+      Thursday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+      Triday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+    };
   });
